14edcffe7512ac3cf539f6460e8b4081a1f2ef55
Nishad P <nishadntechit@gmail.com>
2022-07-07 19:28:29 +0530

    Changed settings

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = ['www.homez.store']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
